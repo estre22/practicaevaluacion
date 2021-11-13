ab205bda5182b5e7ae5169b57ec0f03ea18a1ba0
estre22 <94200598+estre22@users.noreply.github.com>
2021-11-12 19:07:14 -0800

    Add files via upload

---
+++ operaciones basicas.js
@@ -0,0 +1,21 @@
+var res;
+
+function suma(num1,num2) {
+ res = parseFloat(num1) + parseFloat(num2);
+ document.getElementById('lblResultado').innerHTML = res;
+}
+
+function restar(num1,num2) {
+ res = parseFloat(num1) - parseFloat(num2);
+ document.getElementById('lblResultado').innerHTML = res;
+}
+
+function multiplicacion(num1,num2) {
+ res = parseFloat(num1) * parseFloat(num2);
+ document.getElementById('lblResultado').innerHTML = res;
+}
+
+function division(num1,num2) {
+ res = parseFloat(num1) / parseFloat(num2);
+ document.getElementById('lblResultado').innerHTML = res;
+}
